13c3dc7e8a4919b62cb508c2ae011d2ca45e1cbd
Dhesika S <106578196+dhesika-ts@users.noreply.github.com>
2022-06-15 16:54:16 +0530

    Add files via upload

---
+++ Movie Ticket Booking/src/JavaApplication.java
@@ -0,0 +1,22 @@
+package javaapplication4;
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class JavaApplication4 {
+    Connection conn=null;
+public static Connection ConnecrDb(){
+    try{
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn =DriverManager.getConnection("jdbc:mysql://localhost/java_dbmovies","root","");
+        return conn;
+    }
+    catch(Exception e)
+    {
+        System.out.println(e);
+    }
+    
+return null;
+}
+
+   
+}
